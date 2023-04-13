69aa7f153e58cb932095defc900be3376a8904ae
Samantha Hipple <77363883+Hipples@users.noreply.github.com>
2023-04-13 06:55:34 -0700

    added modal component

--- PE02 - Core Components/my-mscs-app/App.js
+++ PE02 - Core Components/my-mscs-app/App.js
@@ -1,9 +1,77 @@
-import React from 'react';
+import { useState } from 'react';
 import { StyleSheet, ScrollView, Image, TextInput, Text, View } from 'react-native';
 
-const App = () => {
-  const [value, onChangeText] = React.useState('')
+import { Pressable, Modal } from 'react-native';
 
+const App = () => {
+  const [value, onChangeText] = useState('');
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const coreCourses = [
+    "CS 504 Software Engineering",
+    "CS 506 Programming for Computing",
+    "CS 519 Cloud Computing Overview",
+    "CS 533 Computer Architecture",
+    "CS 547 Secure Systems and Programs",
+    "CS 622 Discrete Math and Algorithms for Computing",
+    "DS 510 Artificial Intelligence for Data Science",
+    "DS 620 Machine Learning & Deep Learning"
+  ]
+
+  const depthOfStudy = [
+    "CS 624 Full-Stack Development - Mobile App",
+    "CS 628 Full-Stack Development - Web App"
+  ]
+
+  const CoreList = () => {
+    return(
+      coreCourses.map((data, index) => {
+        return(
+          <View key={index}>
+            <Modal
+              animationType='none'
+              transparent={true}
+              visible={modalVisible}
+              onRequestClose={() => {setModalVisible(false)}}
+            >
+              <View style={styles.centeredView}>
+                <View style={styles.modalView}>
+                  <Text>{data}{"\n"}</Text>
+                <Pressable onPress={() => setModalVisible(false)}>
+                  <Text>
+                    {"\n"}Close Modal
+                  </Text>
+                </Pressable>
+              </View>
+              </View>
+            </Modal>
+            <Pressable onPress={() => setModalVisible(true)}>
+              <Text style={styles.courses}>{data}</Text>
+            </Pressable>
+          </View>
+        )
+      })
+    )
+  }
+
+  const DepthList = () => {
+    return(
+      depthOfStudy.map((data, index) => {
+        return(
+          <View key={index}>
+            <Pressable
+              onPress={() => {}}
+            >
+              <Text style={styles.courses}>{data}</Text>
+            </Pressable>
+          </View>
+        )
+      })
+    )
+  }
+
+
+  
   return (
     <ScrollView contentContainerStyle={styles.container}>
 
@@ -32,7 +100,7 @@ const App = () => {
 
       <View>
         <Text style={styles.header}>Core Requirements (24 credits)</Text>
-        <Text style={styles.courses}>
+        {/* <Text style={styles.courses}>
           CS 504 Software Engineering{'\n'}
           CS 506 Programming for Computing{'\n'}
           CS 519 Cloud Computing Overview{'\n'}
@@ -41,12 +109,17 @@ const App = () => {
           CS 622 Discrete Math and Algorithms for Computing{'\n'}
           DS 510 Artificial Intelligence for Data Science{'\n'}
           DS 620 Machine Learning & Deep Learning
-        </Text>
+        </Text> */}
+
+        <CoreList />
+
         <Text style={styles.header}>Depth of Study (6 credits)</Text>
-        <Text style={styles.courses}>
+        {/* <Text style={styles.courses}>
           CS 624 Full-Stack Development - Mobile App{'\n'}
           CS 628 Full-Stack Development - Web App
-        </Text>
+        </Text> */}
+        <DepthList />
+
         <Text style={styles.header}>Capstone (3 credits)</Text>
         <Text style={styles.courses}>CS 687 Computer Science Capstone</Text>
       </View>
@@ -54,6 +127,9 @@ const App = () => {
     </ScrollView>
   )
 }
+
+
+
 
 
 const styles = StyleSheet.create({
@@ -117,6 +193,21 @@ const styles = StyleSheet.create({
     color: 'white',
     marginTop: 5,
     lineHeight: 30
+  },
+
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 40,
+    alignItems: 'center',
+  },
+
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
   }
 });
 
